e247a7dcc5c241552956809b2e961a643a0d2f2e
SAJID ALI <135090212+sajidali23@users.noreply.github.com>
2024-12-13 09:52:55 +0000

    commited

--- app/components/UserMediaCard.tsx
+++ app/components/UserMediaCard.tsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
+import Image from "next/image";
 
-export default function UserMediaCard({userId}:{userId:string}) {
-    return (
-        <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4">
+export default function UserMediaCard({ userId }: { userId: string }) {
+  return (
+    <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4">
       <div className="flex justify-between items-center font-medium">
         <span className="text-gray-500">User Media</span>
         <Link href="/" className="text-blue-500 text-xs">
@@ -11,30 +12,70 @@ export default function UserMediaCard({userId}:{userId:string}) {
       </div>
       <div className="flex gap-4 justify-between flex-wrap">
         <div className="relative w-1/5 h-24">
-            <Image  src="/image-1.jpeg" alt="" fill className="object-cover rounded-md"/>
+          <Image
+            src="/image-1.jpeg"
+            alt=""
+            fill
+            className="object-cover rounded-md"
+          />
         </div>
         <div className="relative w-1/5 h-24">
-            <Image  src="/image-1.jpeg" alt="" fill className="object-cover rounded-md"/>
+          <Image
+            src="/image-1.jpeg"
+            alt=""
+            fill
+            className="object-cover rounded-md"
+          />
         </div>
         <div className="relative w-1/5 h-24">
-            <Image  src="/image-1.jpeg" alt="" fill className="object-cover rounded-md"/>
+          <Image
+            src="/image-1.jpeg"
+            alt=""
+            fill
+            className="object-cover rounded-md"
+          />
         </div>
         <div className="relative w-1/5 h-24">
-            <Image  src="/image-1.jpeg" alt="" fill className="object-cover rounded-md"/>
+          <Image
+            src="/image-1.jpeg"
+            alt=""
+            fill
+            className="object-cover rounded-md"
+          />
         </div>
         <div className="relative w-1/5 h-24">
-            <Image  src="/image-1.jpeg" alt="" fill className="object-cover rounded-md"/>
-        </div> 
+          <Image
+            src="/image-1.jpeg"
+            alt=""
+            fill
+            className="object-cover rounded-md"
+          />
+        </div>
         <div className="relative w-1/5 h-24">
-            <Image  src="/image-1.jpeg" alt="" fill className="object-cover rounded-md"/>
-        </div> 
+          <Image
+            src="/image-1.jpeg"
+            alt=""
+            fill
+            className="object-cover rounded-md"
+          />
+        </div>
         <div className="relative w-1/5 h-24">
-            <Image  src="/image-1.jpeg" alt="" fill className="object-cover rounded-md"/>
-        </div> 
+          <Image
+            src="/image-1.jpeg"
+            alt=""
+            fill
+            className="object-cover rounded-md"
+          />
+        </div>
         <div className="relative w-1/5 h-24">
-            <Image  src="/image-1.jpeg" alt="" fill className="object-cover rounded-md"/>
+          <Image
+            src="/image-1.jpeg"
+            alt=""
+            fill
+            className="object-cover rounded-md"
+          />
         </div>
       </div>
-      </div>
-    )
+    </div>
+  );
 }
