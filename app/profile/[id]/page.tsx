a76280188ea3dfe02c1a80da280bc1c9be1aee23
Latifun Neaha <latifunnesha1000@gmail.com>
2024-12-15 09:34:20 +0000

    coomit

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -1,4 +1,5 @@
 import Feed from "@/app/components/Feed";
+import Image from "next/image";
 import LeftMenu from "@/app/components/LeftMenu";
 import RightMenu from "@/app/components/RightMenu";
 
@@ -10,6 +11,27 @@ export default function ProfilePage() {
       </div>
       <div className="w-full lg:w-[70%] xl:w-[50%]">
         <div className="flex flex-col gap-6">
+          <div className="flex flex-col items-center justify-center">
+            <div className="w-full h-64 relative">
+              <Image src="/image-1.jpeg" alt="" fill className="rounded-md object-cover"/>
+              <Image src="/image-1.jpeg" alt="" width={128} height={128} className="w-32 h-32 rounded-full absolute left-0 right-0 m-auto -bottom-16 ring-4 ring-white object-cover"/>
+            </div>
+            <h1 className="mt-20 mb-4 text-2xl font-medium">Elna Wayne</h1>
+            <div className="flex items-center justify-center gap-12 mb-4">
+              <div className="flex flex-col items-center">
+                <span className="font-medium">123</span>
+                <span className="text-sm">Posts</span>
+              </div>
+              <div className="flex flex-col items-center">
+                <span className="font-medium">100K+ </span>
+                <span className="text-sm">Follower's</span>
+              </div>
+              <div className="flex flex-col items-center">
+                <span className="font-medium">100</span>
+                <span className="text-sm">Following's</span>
+              </div>
+            </div>
+          </div>
           <Feed />
         </div>
       </div>
